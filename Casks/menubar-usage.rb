3251285a43c18ae2d04fdb22a60f64865cbb9b6b
Miffy Chen <mchen15@bu.edu>
2026-05-28 21:25:50 -0700

    Update sha256 for rebuilt v0.1.0

--- Casks/menubar-usage.rb
+++ Casks/menubar-usage.rb
@@ -6,7 +6,7 @@
 
 cask "menubar-usage" do
   version "0.1.0"
-  sha256 "6c4fc43ea8927a2f20bd60d74a40135de52f355ded8334ea042fbdd725dfd168"
+  sha256 "ab51c89bb2e7518d11a4d6f63407434343ff0a9cc959dc55f399ea048077cc7f"
 
   url "https://github.com/miffycs/menubar-usage/releases/download/v#{version}/menubar-usage.app.zip"
   name "Menubar Usage"
